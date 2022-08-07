0a0f6be76f834d47d16181e8ee1cbf59b50307db
guormet <1445190395@qq.com>
2022-08-07 14:39:21 +0800

    打通mongoDB/redis配置，并测试

---
+++ api/src/config/index.js
@@ -0,0 +1,13 @@
+const DB_URL = 'mongodb://test:123456@120.77.34.116:27017/testdb'
+
+const REDIS = {
+  host: '120.77.34.116',
+  port: 15001,
+  password: '123456'
+}
+
+
+export default {
+  DB_URL,
+  REDIS,
+}
---
+++ api/src/model/demo.js
@@ -0,0 +1,37 @@
+import User from './test'
+
+// 增
+// const user = {
+//   name: 'keaton',
+//   age: 30,
+//   email: '1445190395@qq.com'
+// }
+
+// const addUser = async () => {
+//   const data = new User(user)
+//   const result = await data.save()
+//   console.log(result);
+// }
+
+// 删
+// const deleteUser = async () => {
+//   const result = await User.deleteOne({ name: 'keaton' })
+//   console.log(result);
+// }
+// 改
+// const updateUser = async () => {
+//   const result = await User.updateOne({ name: 'keaton' }, {
+//     age: 29,
+//     email: '1445190359@qq.com'
+//   })
+//   console.log(result);
+// }
+// 查
+// const selectUser = async () => {
+//   const result = await User.find()
+//   console.log(result);
+// }
+// addUser()
+// selectUser()
+// updateUser()
+// deleteUser()
